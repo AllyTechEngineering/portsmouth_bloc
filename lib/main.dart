b47ec86946f966832d1740f1221874aaca855536
Admin <btaylor@allytechllc.com>
2023-10-29 14:52:39 -0400

    Snapshot before I change to the simpsons.json as the data model

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class MyApp extends StatelessWidget {
             repository: CenterboardRepository(
               Dio(),
             ),
-            searchCubit: SearchCubit(),
+            searchCubit: context.read<SearchCubit>(),
           ),
         ),
       ],
